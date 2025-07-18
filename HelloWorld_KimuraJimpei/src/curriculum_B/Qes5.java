84721a2d6791f2067a73ea020ec35e7229eb803b
zinn <zinn@DESKTOP-129S3VA>
2025-07-18 21:15:29 +0900

    Qes5 ok

--- HelloWorld_KimuraJimpei/src/curriculum_B/Qes5.java
+++ HelloWorld_KimuraJimpei/src/curriculum_B/Qes5.java
@@ -1,10 +1,26 @@
 package curriculum_B;
 
 public class Qes5 {
+	
+	/*問題文
+	 * 001 * 001 = 001 || 001 * 002 = 002 || … 020 * 009 = 180 のように出力
+	 * Qes4の改変でOK
+	 */
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// 掛ける数
+		for (int i = 1; i <= 20; i++) {
+			// 掛けられる数
+			for (int j = 1; j <= 9; j++) {
+				System.out.printf("%03d * %03d = %03d", i, j, i * j);//%書式文字列、0埋める文字、3桁数、d値の型(整数)
 
+				// jが9ではない場合「 || 」を付ける
+				if (j != 9) {
+					System.out.print(" || ");
+				}
+			}
+			//改行
+			System.out.println();
+		}
 	}
-
 }
