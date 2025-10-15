a6cb987c3126a45ef6f32284833a2b4124890233
zinn <zinn@DESKTOP-129S3VA>
2025-10-15 19:12:26 +0900

    Qes1.31 ok

---
+++ Qes1.30-1.32/src/selfIntroduce2/Main.java
@@ -0,0 +1,16 @@
+package selfIntroduce2;
+
+class Main {
+
+	public static void main(String[] argos) {
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+		person1.print();
+		Person person2 = new Person("山田花子", 22, 1.5, 40);
+		person2.print();
+		//直接クラスフィールドcountを出力
+		System.out.println("合計" + Person.count + "人です");
+		//クラスメソッドprintCountの呼び出し
+		Person.printCount();
+	}
+
+}
